bd28bc555a2786c93605162c7576fe43ccb0d201
albara <albaraalhoweml@gmail.com>
2019-11-14 23:59:41 +0300

    first commit

--- app/src/main/java/com/example/cameracustom/MainActivity.java
+++ app/src/main/java/com/example/cameracustom/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.cameracustom;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,8 +26,11 @@ public class MainActivity extends AppCompatActivity {
     Camera camera;
     FrameLayout frameLayout;
     TextView textViewX, textViewY, textViewZ;
+//    ImageView imageView;
     Button button;
     Show show;
+    static Bitmap bitmap;
+    Camera.PictureCallback mPicture;
     final private int MY_REQUEST_CODE = 123;
 
     /* TBA
@@ -41,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         frameLayout = findViewById(R.id.camera_preview);
+//        imageView = findViewById(R.id.imageView);
         textViewX = findViewById(R.id.textViewX);
         textViewY = findViewById(R.id.textViewY);
         textViewZ = findViewById(R.id.textViewZ);
@@ -53,21 +62,22 @@ public class MainActivity extends AppCompatActivity {
         // Create an instance of camera
         camera = getCameraInstance();
         camera.setDisplayOrientation(90);
+
+
         // Create Camera preview to set our activity
         show = new Show(this,camera);
         frameLayout.addView(show);
-        CheckUserPermissions();
+        CheckCameraPermissions();
+
 
         // to autofocus
         Camera.Parameters params = camera.getParameters();
         params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
         camera.setParameters(params);
 
-        //to register sensor
-        sensorManager.registerListener(rvListener, rotationSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-
-
+    }
+    public void onClick(View view){
+        camera.takePicture(null, null, picture);
     }
 
     // Create a listener
@@ -87,18 +97,15 @@ public class MainActivity extends AppCompatActivity {
                 orientations[i] = (float)(Math.toDegrees(orientations[i]));
             }
 
-//            textViewX.setText("X: " + orientations[0]);
+            textViewX.setText("X: " + orientations[0]);
             textViewY.setText("Y: " +orientations[1]);
             textViewZ.setText("Z: "+orientations[2]);
 
-            // take pic in 90 degree
+//             take pic in 90 degree
             if ((orientations[2] < 1 & orientations[2] > -1) && (orientations[1] < 1 & orientations[1] > -1) ){
                 button.setVisibility(View.VISIBLE);
             }else
                 button.setVisibility(View.GONE);
-//            textViewX.setText("X: " + sensorEvent.values[0]);
-//            textViewY.setText("Y: " +sensorEvent.values[1]);
-//            textViewZ.setText("Z: "+sensorEvent.values[2]);
     }
 
         @Override
@@ -107,15 +114,37 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
-    public void onClick(View view){
+//        go to upload activity
+        Camera.PictureCallback picture = new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                Intent intent = new Intent(MainActivity.this,PictureActivity.class);
+                startActivity(intent);
+            }
+        };
 
+    private static Camera openFrontFacingCamera()
+    {
+        int cameraCount = 0;
+        Camera cam = null;
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        cameraCount = Camera.getNumberOfCameras();
+        for ( int camIdx = 0; camIdx < cameraCount; camIdx++ ) {
+            Camera.getCameraInfo( camIdx, cameraInfo );
+            if ( cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT  ) {
+                try {
+                    cam = Camera.open( camIdx );
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return cam;
     }
 
-
-
-
-
-    void CheckUserPermissions() {
+    void CheckCameraPermissions() {
         if (Build.VERSION.SDK_INT >= 23) {
             // Camera permission
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
@@ -125,21 +154,13 @@ public class MainActivity extends AppCompatActivity {
                         MY_REQUEST_CODE);
                 return;
             }
-            // Storage permission
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
-                    PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        MY_REQUEST_CODE);
-                return;
-            }
         }
     }
 
     public static Camera getCameraInstance(){
         Camera c = null;
         try {
-            c = Camera.open(); // attempt to get a Camera instance
+            c = openFrontFacingCamera(); // attempt to get a Camera instance
         }
         catch (Exception e){
             // Camera is not available (in use or does not exist)
---
+++ app/src/main/java/com/example/cameracustom/PictureActivity.java
@@ -0,0 +1,145 @@
+package com.example.cameracustom;
+
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PictureActivity extends AppCompatActivity {
+
+    private ImageView camera_preview;
+    private Button button;
+    private String url = "http://IP192.168  :3000/upload/upload.php";
+    private static final String IMAGE_DIRECTORY = "/CustomImage";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        camera_preview = findViewById(R.id.camera_preview);
+        button = findViewById(R.id.button);
+
+        camera_preview.setImageBitmap(MainActivity.bitmap);
+
+        saveImage(MainActivity.bitmap);
+    }
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+//                            }
+//
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(getApplicationContext(), "error: "+ error.toString(), Toast.LENGTH_LONG).show();
+//                    }
+//                }){
+//                    @Override
+//                    protected Map<String, String> getParams() throws AuthFailureError {
+//                        Map<String, String> params = new HashMap<>();
+//                        String imageData = imageToString(bitmap);
+//                        params.put("image",imageData);
+//
+//                        return params;
+//                    }
+//                };
+//                RequestQueue requestQueue = Volley.newRequestQueue(PictureActivity.this);
+//                requestQueue.add(stringRequest);
+//            }
+//        });
+//    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+
+        if (!wallpaperDirectory.exists()) {
+            Log.d("dirrrrrr", "" + wallpaperDirectory.mkdirs());
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();   //give read write permission
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+
+    }
+
+    public void onClick(View view){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "error: "+ error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                String imageData = imageToString(MainActivity.bitmap);
+                params.put("image",imageData);
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(PictureActivity.this);
+        requestQueue.add(stringRequest);
+    }
+
+    public String imageToString(Bitmap bitmap){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte [] imgBytes =  byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imgBytes,Base64.DEFAULT);
+    }
+}
